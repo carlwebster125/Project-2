ce6fef43954ebc4f64179a5dcbb061d0581a9866
Carl Webster <carlwebster125@gmail.com>
2023-10-26 20:25:34 -0400

    basic structure for P2

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const port = 3000;
+
+const es6Renderer = require('express-es6-template-engine');
+const {Character} = require('./models');
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    const characters = await Character.findAll()
+    res.render('template', {
+        locals: {
+            character: characters[0],
+        }
+    })});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ seeders/20231026232338-user.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstName: "John",
+          lastName: "Smith",
+          email: "jsmith@test.com",
+          password: "password123",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
