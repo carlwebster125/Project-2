bc9ded24a7b7f2392671de5257e8b713d7857234
Mia Morris <M.Morris5@yahoo.com>
2023-11-06 23:32:06 -0500

    ok

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const server = http.createServer(app);
 const port = 3000;
 const characterRouter = require('./routes/characters');
+const userRouter = require('./routes/user')
 
 const es6Renderer = require('express-es6-template-engine');
 app.engine('html', es6Renderer);
@@ -13,7 +14,9 @@ app.set('view engine', 'html');
 app.use(characterRouter);
 app.use(express.static('public'));
 app.use(express.json());
+app.use(indexRouter);
 app.use(userRouter);
+
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
