e6396cbac15aad195bd1228c45649dc0f5f9c829
Mia Morris <M.Morris5@yahoo.com>
2023-11-07 01:11:04 -0500

    lmnop

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const http = require('http');
 const express = require('express');
+const session = require('express-session')
+const userRouter = require('./routes/user')
+const es6Renderer = require('express-es6-template-engine');
+
 const app = express();
 const server = http.createServer(app);
-const port = 3000;
-const characterRouter = require('./routes/characters');
-const userRouter = require('./routes/user')
+const port = process.env.PORT;
 
-const es6Renderer = require('express-es6-template-engine');
+const characterRouter = require('./routes/characters');
+
+
+const db = require('./models');
+
 app.engine('html', es6Renderer);
 app.set('views', 'views');
 app.set('view engine', 'html');
@@ -14,6 +20,7 @@ app.set('view engine', 'html');
 app.use(characterRouter);
 app.use(express.static('public'));
 app.use(express.json());
+
 app.use(indexRouter);
 app.use(userRouter);
 
